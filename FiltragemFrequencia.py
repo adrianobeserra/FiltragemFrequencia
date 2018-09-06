77ea2504f7a49ccd7c82140c8db05cb0f04b5bab
Carlos Adriano <adrianobeserra@gmail.com>
2018-09-05 22:29:12 -0300

    Comentários em cada passo

--- FiltragemFrequencia.py
+++ FiltragemFrequencia.py
@@ -8,10 +8,11 @@ def DFT(x):
     """Calcula a transformada discreta de Fourier 2D"""
     """Compute the discrete Fourier Transform of the 1D array x"""
     x = np.asarray(x, dtype=complex)
+
     N = x.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
-    M = np.exp(2j * np.pi * k * n / N)
+    M = np.exp(-2j * np.pi * k * n / N)
     return np.dot(M, x)
 
 def DFT_inversa(x):
@@ -24,67 +25,29 @@ def DFT_inversa(x):
     M = 1/N*(np.exp(-2j * np.pi * k * n / N))
     return np.dot(M, x)
 
-def getFiltro(tamanhoJanela):
-    filtro = np.zeros((tamanhoJanela, tamanhoJanela), np.double)
-    return filtro
-
 def DFT_main(img):
     height, width = img.shape[0], img.shape[1]
     P,Q = 2*width, 2*height
-    #imgDest = np.zeros((height, width), dtype=complex)
-    imgDest = np.zeros((height, width), dtype=np.double)
+
+    #Gera uma Imagem [2M, 2N] completada com zeros. Passo 1
     img_preenchida = np.zeros((Q, P), dtype=np.double)
-    filtro = getFiltro(3)
-    filter_width, filter_height = filtro.shape[0], filtro.shape[1]
 
-    filtro[0,0] = 1
-    filtro[0,1] = 1
-    filtro[0,2] = 1
-    filtro[1,0] = 1
-    filtro[1,1] = -8
-    filtro[1,2] = 1
-    filtro[2,0] = 1
-    filtro[2,1] = 1
-    filtro[2,2] = 1
-
+    #Gera uma imagem preenchida contendo a imagem original. Passo 2
     for y in range(height-1):
         for x in range(width-1):
             img_preenchida[y,x] = img[y,x]
 
+    #Centralizar a transformada multiplicando a imagem por (-1)**(y+x). Passo 3
     for y in range(Q-1):
         for x in range(P-1):
             img_preenchida[y,x] = img_preenchida[y,x]*(-1)**(y+x)
 
-    for y in range(Q):
+    #Calculando a transformada discreta de fourier. Passo 4
+    for y in range(Q-1):
         row = img_preenchida[y,: ]
         row = DFT(row)
         img_preenchida[y,: ] = row.real
 
-    for y in range(height):
-        for x in range(width):
-            sum_pixels = 0
-            for filterY in range(int(-(filter_height / 2)), filter_height - 1):
-                for filterX in range(int(-(filter_width / 2)), filter_width - 1):
-                    pixel_y = y - filterY
-                    pixel_x = x - filterX
-                    pixel = img_preenchida[filterY, filterX]
-
-                    #Defini a ação a ser realizada quando o pixel atual fizer parte das bordas da imagem.
-                    if (pixel_y >= 0) and (pixel_y < height) and (pixel_x >= 0) and (pixel_x < width):
-                        pixel = img_preenchida[pixel_y, pixel_x]
-
-                    img_preenchida[filterY, filterX] = img_preenchida[filterY, filterX] * filtro[filterY, filterX]
-
-    for y in range(Q):
-        row = img_preenchida[y,: ]
-        row = DFT_inversa(row)
-        img_preenchida[y,: ] = row.real
-
-    for y in range(Q-1):
-        for x in range(P-1):
-            img_preenchida[y,x] = img_preenchida[y,x]*(-1)**(y+x)
-
-    return img_preenchida
 
 def process_image(imgName):
     start_time = time.time()
@@ -110,4 +73,4 @@ def process_image(imgName):
     '''
 
 '''Programa Principal'''
-process_image("Agucar_(1).jpg")
+process_image("2.jpg")
