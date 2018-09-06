79f82fe3f141a35c29406974c2c0082bae624570
Carlos Adriano <adrianobeserra@gmail.com>
2018-09-06 03:58:15 -0300

    Versão Final

--- FiltragemFrequencia.py
+++ FiltragemFrequencia.py
@@ -3,12 +3,12 @@ import numpy as np
 import time
 import sys
 import os
+import math
 
 def DFT(x):
     """Calcula a transformada discreta de Fourier 2D"""
     """Compute the discrete Fourier Transform of the 1D array x"""
     x = np.asarray(x, dtype=complex)
-
     N = x.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
@@ -22,7 +22,7 @@ def DFT_inversa(x):
     N = x.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
-    M = 1/N*(np.exp(-2j * np.pi * k * n / N))
+    M = 1/N*(np.exp(2j * np.pi * k * n / N))
     return np.dot(M, x)
 
 def DFT_main(img):
@@ -30,7 +30,8 @@ def DFT_main(img):
     P,Q = 2*width, 2*height
 
     #Gera uma Imagem [2M, 2N] completada com zeros. Passo 1
-    img_preenchida = np.zeros((Q, P), dtype=np.double)
+    img_preenchida = np.zeros((Q, P), dtype=np.complex)
+    img_processada = np.zeros((Q, P), dtype=np.double)
 
     #Gera uma imagem preenchida contendo a imagem original. Passo 2
     for y in range(height-1):
@@ -46,8 +47,35 @@ def DFT_main(img):
     for y in range(Q-1):
         row = img_preenchida[y,: ]
         row = DFT(row)
-        img_preenchida[y,: ] = row.real
+        img_preenchida[y,: ] = row
 
+    #Aplicando a função filtro. Passo 5
+    for y in range(Q-1):
+        for x in range(P-1):
+            if (x != 0 and y != 0):
+                #img_preenchida[y,x] = img_preenchida[y,x] * (1/(1 + (30 / (math.sqrt(y**2 + x**2)))**2*2))
+                D = math.sqrt(y**2 + x**2)
+                img_preenchida[y,x] = img_preenchida[y,x] * (1 - np.exp(-D**2 / 2 * 160**2))
+
+    #Calculando a transformada discreta inversa de fourier. Passo 6
+    for y in range(Q-1):
+        row = img_preenchida[y,: ]
+        row = DFT_inversa(row)
+        img_preenchida[y,: ] = row
+
+    #Obtendo a parte real da imagem processada. Passo 7
+    for y in range(Q-1):
+        for x in range(P-1):
+            img_processada[y,x] = img_preenchida[y,x].real * (-1)**(y+x)
+
+
+    img_final = np.zeros((height, width), dtype=np.double)
+    #Voltando as dimensões originais da imagem. Passo 7
+    for y in range(height-1):
+        for x in range(width-1):
+            img_final[y,x] = img_processada[y,x]
+
+    return img_final
 
 def process_image(imgName):
     start_time = time.time()
@@ -73,4 +101,8 @@ def process_image(imgName):
     '''
 
 '''Programa Principal'''
-process_image("2.jpg")
+process_image("Agucar_(1).jpg")
+process_image("Agucar_(2).jpg")
+process_image("Agucar_(3).jpg")
+process_image("Agucar_(4).jpg")
+process_image("Agucar_(5).jpg")
